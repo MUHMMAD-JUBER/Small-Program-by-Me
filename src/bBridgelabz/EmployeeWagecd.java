a991d344be1fb727d71c0203ea46091ee1f4902d
MUHMMAD-JUBER <150322620+MUHMMAD-JUBER@users.noreply.github.com>
2023-11-15 16:10:43 +0530

    Added :all small codes done y apna code

---
+++ src/bBridgelabz/EmployeeWagecd.java
@@ -0,0 +1,35 @@
+package bBridgelabz;
+
+/*static final IS_FULL_TIME=1;
+static final IS_PART_TIME=2;
+
+public class EmployeeWagecd {
+    public static void main(String args[]) {
+        int empHrs = 0, empWage=0;
+        int EMP_RATE_PER_HOUR=125;
+        for(int day=1;day<NUM_WORKING_DAYS;day++){
+        double empCheck = Math.floor(Math.random()*10)%3;
+        System.out.println("Employee Attendence Check "+empCheck);
+        switch ((int) (empCheck){
+        case IS_FULL_TIME:
+            System.out.println("Employee is Present ");
+            empHrs=8;
+            break;
+        case IS_FULL_TIME:
+            System.out.println("Employee is Present ");
+            empHrs=8;
+        default:
+            empHrs=0;
+        }
+        //else if (empCheck==IS_FULL_TIME){
+        //         System.out.println("Employee is Present & Part Time ");
+        //         empHrs=4;
+        //     }
+        //     else {
+        //         System.out.println(" Employee is Absent");
+        //         empHrs = 0;
+        //     }
+        //     empWage = empHrs*EMP_RATE_PER_HOUR;
+        //     System.out.println("Employee Wage is "+empWage);
+        // }
+    }}*/
